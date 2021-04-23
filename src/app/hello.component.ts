5e05cfa4c05476b9d42d7ab21357643805e7ead1
JMBratten <jbratten@agvance.net>
2021-04-23 16:12:23 -0500

    rxjs ajax

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ajax } from 'rxjs/ajax';
+
+export enum e {
+  value1 = 1,
+  value2 = 2
+};
 
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`]
 })
-export class HelloComponent  {
+export class HelloComponent implements OnInit {
   @Input() name: string;
+
+  ngOnInit() {
+    const githubUsers = `https://api.github.com/users?per_page=2`;
+
+    const users = ajax.getJSON(githubUsers);
+
+    const subscribe = users.subscribe(
+      res => console.log(res),
+      err => console.error(err)
+    );
+  }
 }
